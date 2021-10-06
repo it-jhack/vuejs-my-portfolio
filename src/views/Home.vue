677beb2a5a90d78846d305b86788cd670f99a2b2
Thiago Amaral <thiago.amaral@outlook.com>
2021-10-06 07:25:13 -0300

    template merge

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,17 @@
 <template>
-  <hello-world />
+  <div>
+    <!-- <hello-world /> -->
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+// import HelloWorld from "../components/HelloWorld";
 
 export default {
   name: "Home",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
