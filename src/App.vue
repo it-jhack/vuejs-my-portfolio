b7ac85695fcb2c27943a4ce1f7e5a5c12ba05ab2
thiago.amaral <thiago.amaral@outlook.com>
2022-07-21 09:16:21 -0300

    FIX del scroll-top button (laggy on mobiles)

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,6 @@
       <experience />
       <contact />
     </v-main>
-    <v-scale-transition>
-      <v-btn
-        fab
-        v-show="fab"
-        v-scroll="onScroll"
-        dark
-        fixed
-        bottom
-        right
-        color="secondary"
-        @click="toTop"
-      >
-        <v-icon>mdi-arrow-up</v-icon>
-      </v-btn>
-    </v-scale-transition>
     <myfooter />
   </v-app>
 </template>
@@ -47,43 +32,6 @@ export default {
     projects,
     experience,
     contact,
-  },
-
-  data: () => ({
-    fab: null,
-    color: "",
-    flat: null,
-  }),
-
-  created() {
-    const top = window.pageYOffset || 0;
-    if (top <= 60) {
-      this.color = "transparent";
-      this.flat = true;
-    }
-  },
-
-  watch: {
-    fab(value) {
-      if (value) {
-        this.color = "secondary";
-        this.flat = false;
-      } else {
-        this.color = "transparent";
-        this.flat = true;
-      }
-    },
-  },
-
-  methods: {
-    onScroll(e) {
-      if (typeof window === "undefined") return;
-      const top = window.pageYOffset || e.target.scrollTop || 0;
-      this.fab = top > 60;
-    },
-    toTop() {
-      this.$vuetify.goTo(0);
-    },
   },
 };
 </script>
