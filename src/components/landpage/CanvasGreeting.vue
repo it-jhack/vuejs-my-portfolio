ae64c818b02c22dc59d0c943f7002b53c8a10487
thiago.amaral <thiago.amaral@outlook.com>
2022-08-02 06:28:48 -0300

    FEAT commented canvas webgl fluid simulation property options

--- src/components/landpage/CanvasGreeting.vue
+++ src/components/landpage/CanvasGreeting.vue
@@ -43,6 +43,37 @@ import WebGLFluid from "webgl-fluid";
 export default {
   mounted() {
     WebGLFluid(this.$refs.canvas);
+
+    // Edit canvas WebGL Fluid Simulation Properties: see more at https://www.npmjs.com/package/webgl-fluid
+    // WebGLFluid(document.querySelector('canvas'), {
+    //   IMMEDIATE: true, // Whether to trigger multiple random splats when initialized
+    //   TRIGGER: 'hover', // Can be change to 'click'
+    //   SIM_RESOLUTION: 128,
+    //   DYE_RESOLUTION: 1024,
+    //   CAPTURE_RESOLUTION: 512,
+    //   DENSITY_DISSIPATION: 1,
+    //   VELOCITY_DISSIPATION: 0.2,
+    //   PRESSURE: 0.8,
+    //   PRESSURE_ITERATIONS: 20,
+    //   CURL: 30,
+    //   SPLAT_RADIUS: 0.25,
+    //   SPLAT_FORCE: 6000,
+    //   SHADING: true,
+    //   COLORFUL: true,
+    //   COLOR_UPDATE_SPEED: 10,
+    //   PAUSED: false,
+    //   BACK_COLOR: { r: 0, g: 0, b: 0 },
+    //   TRANSPARENT: false,
+    //   BLOOM: true,
+    //   BLOOM_ITERATIONS: 8,
+    //   BLOOM_RESOLUTION: 256,
+    //   BLOOM_INTENSITY: 0.8,
+    //   BLOOM_THRESHOLD: 0.6,
+    //   BLOOM_SOFT_KNEE: 0.7,
+    //   SUNRAYS: true,
+    //   SUNRAYS_RESOLUTION: 196,
+    //   SUNRAYS_WEIGHT: 0.75,
+    // })
   },
 };
 </script>
