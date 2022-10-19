74685a60e084e71307c888e3ab798827f8f3c4b6
thiago.amaral <thiago.amaral@outlook.com>
2022-10-19 08:09:12 -0300

    feat: remove skill progress bar for idioms

--- src/components/landpage/AboutMe.vue
+++ src/components/landpage/AboutMe.vue
@@ -33,7 +33,7 @@
 
               <v-expansion-panels dark multiple>
                 <v-expansion-panel>
-                  <v-expansion-panel-header class="skills-dropdown">
+                  <v-expansion-panel-header>
                     Hard Skills
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
@@ -87,7 +87,7 @@
                 </v-expansion-panel>
 
                 <v-expansion-panel>
-                  <v-expansion-panel-header class="skills-dropdown">
+                  <v-expansion-panel-header>
                     Soft Skills
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
@@ -145,123 +145,40 @@
                 </v-expansion-panel>
 
                 <v-expansion-panel>
-                  <v-expansion-panel-header class="skills-dropdown">
+                  <v-expansion-panel-header>
                     Idioms
                   </v-expansion-panel-header>
                   <v-expansion-panel-content>
-                    <div class="skills">
+                    <div>
                       <p class="idiom-title">
                         <img
                           src="@/assets/img/brazil_icon.jpg"
-                          max-height="40px"
-                          max-width="30px"
-                        />&nbsp; Brazilian Portuguese (Native)
+                          class="idiom-flag"
+                        />
+                        <span class="ml-2">
+                          Brazilian Portuguese (Native)
+                        </span>
                       </p>
-                      <ul class="skill-ul">
-                        <li class="skill-li">
-                          <span class="skill-span pt-r"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Reading:</span> Excellent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span pt-w"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Writing:</span>
-                            Excellent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span pt-l"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Listening:</span>
-                            Excellent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span pt-s"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Speaking:</span>
-                            Excellent
-                          </p>
-                        </li>
-                      </ul>
 
                       <p class="idiom-title">
                         <img
                           src="@/assets/img/us-icon.png"
-                          max-height="40px"
-                          max-width="30px"
-                        />&nbsp; English (Fluent)
+                          class="idiom-flag"
+                        />
+                        <span class="ml-2">
+                          English (Fluent)
+                        </span>
                       </p>
-                      <ul class="skill-ul">
-                        <li class="skill-li">
-                          <span class="skill-span en-r"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Reading:</span>
-                            Fluent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span en-w"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Writing:</span>
-                            Fluent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span en-l"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Listening:</span>
-                            Fluent
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span en-s"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Speaking:</span>
-                            Fluent
-                          </p>
-                        </li>
-                      </ul>
 
                       <p class="idiom-title">
                         <img
                           src="@/assets/img/spain_flag.png"
-                          max-height="40px"
-                          max-width="30px"
-                        />&nbsp; Spanish (Good)
+                          class="idiom-flag"
+                        />
+                        <span class="ml-2">
+                          Spanish (Communicable)
+                        </span>
                       </p>
-                      <ul class="skill-ul">
-                        <li class="skill-li">
-                          <span class="skill-span sp-r"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Reading:</span>
-                            Very Good
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span sp-w"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Writing:</span>
-                            Very Good
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span sp-l"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Listening:</span>
-                            Good
-                          </p>
-                        </li>
-                        <li class="skill-li">
-                          <span class="skill-span sp-s"></span>
-                          <p class="skill-text">
-                            <span class="skill-bold">Speaking:</span>
-                            Communicable
-                          </p>
-                        </li>
-                      </ul>
                     </div>
                   </v-expansion-panel-content>
                 </v-expansion-panel>
@@ -306,194 +223,12 @@ html {
 }
 
 .idiom-title {
-  font-size: 22px;
-}
-
-.skills-dropdown {
   font-size: 16px;
 }
 
-/* Skill Bars */
-.skills {
-  width: 93%;
-  position: relative;
-  float: left;
-  padding: 15px 0;
-}
-.skill-ul {
-  padding-top: 30px;
-}
-.skill-li {
-  margin-bottom: 50px;
-  background: #dededed5;
-  height: 6px;
-  list-style-type: none;
-}
-.skill-text {
-  font-style: italic;
-  font-size: 18px;
-  position: relative;
-  top: -30px;
-}
-.skill-bold {
-  font-weight: 600;
-}
-.skill-span {
-  height: inherit;
-  background-color: #009600da;
-  /* background: linear-gradient(90deg, #009600da, #009600da); */
-  background: #009600da;
-  position: absolute;
-}
-
-.pt-r {
-  width: 96.2%;
-  animation: pt-r 1s ease-out;
-}
-.pt-w {
-  width: 96.2%;
-  animation: pt-w 1s ease-out;
-}
-.pt-l {
-  width: 96.2%;
-  animation: pt-l 1s ease-out;
-}
-.pt-s {
-  width: 96.2%;
-  animation: pt-s 1s ease-out;
-}
-
-.en-r {
-  width: 93%;
-  animation: en-r 1s ease-out;
-}
-.en-w {
-  width: 93%;
-  animation: en-w 1s ease-out;
-}
-.en-l {
-  width: 93%;
-  animation: en-l 1s ease-out;
-}
-.en-s {
-  width: 93%;
-  animation: en-s 1s ease-out;
-}
-
-.sp-r {
-  width: 85%;
-  animation: sp-r 1s ease-out;
-}
-.sp-w {
-  width: 80%;
-  animation: sp-w 1s ease-out;
-}
-.sp-l {
-  width: 70%;
-  animation: sp-l 1s ease-out;
-}
-.sp-s {
-  width: 60%;
-  animation: sp-s 1s ease-out;
-}
-
-/* idioms */
-@keyframes pt-r {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 93%;
-  }
-}
-@keyframes pt-w {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 93%;
-  }
-}
-@keyframes pt-l {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 93%;
-  }
-}
-@keyframes pt-s {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 93%;
-  }
-}
-
-@keyframes en-r {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 90%;
-  }
-}
-@keyframes en-w {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 90%;
-  }
-}
-@keyframes en-l {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 90%;
-  }
-}
-@keyframes en-s {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 90%;
-  }
-}
-
-@keyframes sp-r {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 85%;
-  }
-}
-@keyframes sp-w {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 80%;
-  }
-}
-@keyframes sp-l {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 70%;
-  }
-}
-@keyframes sp-s {
-  0% {
-    width: 0px;
-  }
-  100% {
-    width: 60%;
-  }
+.idiom-flag {
+  max-height: 40px;
+  max-width: 30px;
+  transform: translate(0, 4px)
 }
 </style>
